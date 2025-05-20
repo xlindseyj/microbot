d0011276e2125dd1e758c32625ac4586d2f690c8
Jake Lindsey <xlindseyj@outlook.com>
2025-05-20 16:51:59 -0400

    Delete plugin

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/KromitePlugins/kgildedaltar/GildedAltarPlayerState.java
+++
@@ -1,9 +0,0 @@
-package net.runelite.client.plugins.microbot.KromitePlugins.kgildedaltar;
-
-public enum GildedAltarPlayerState {
-    LEAVE_HOUSE,
-    UNNOTE_BONES,
-    ENTER_HOUSE,
-    BONES_ON_ALTAR,
-    IDLE,
-}
